e0c892df7c14962f6e4fd61a4ca584a810877acd
Ahmet Birsen <ahmetbrsn44@hotmail.com>
2024-02-12 14:20:31 +0300

    Product repository interface and implementatiton created.

--- app/src/main/java/com/example/momentcommerce/data/repo/ProductRepositoryImpl.kt
+++ app/src/main/java/com/example/momentcommerce/data/repo/ProductRepositoryImpl.kt
@@ -1,13 +1,66 @@
 package com.example.momentcommerce.data.repo
 
-import com.example.momentcommerce.data.source.ProductsJsonSource
+import com.example.momentcommerce.data.source.file.ProductsJsonSource
+import com.example.momentcommerce.data.source.room.CommerceDao
+import com.example.momentcommerce.data.source.room.CommerceRoomSource
+import com.example.momentcommerce.model.BagProduct
+import com.example.momentcommerce.model.LikedProduct
 import com.example.momentcommerce.model.Product
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ProductRepositoryImpl @Inject constructor(
-    private val productJsonSource : ProductsJsonSource
+    private val productJsonSource : ProductsJsonSource,
+    private val productRoomSource: CommerceRoomSource
 ): ProductsRepository {
     override suspend fun getProducts(): List<Product> {
         return productJsonSource.getProducts()
     }
+
+    override suspend fun insertProduct(productBag: BagProduct) {
+        productRoomSource.insertProduct(productBag)
+    }
+
+    override suspend fun insertLikedProduct(likedProduct: LikedProduct) {
+        productRoomSource.insertLikedProduct(likedProduct)
+    }
+
+    override suspend fun deleteLikedProduct(productID: Int) {
+        productRoomSource.deleteLikedProduct(productID)
+    }
+
+    override suspend fun updateProduct(
+        productID: Int,
+        newProductCount: Int,
+        newProductAmount: Double
+    ) {
+        productRoomSource.updateProduct(productID, newProductCount, newProductAmount)
+    }
+
+    override suspend fun deleteProduct(productID: Int) {
+        productRoomSource.deleteProduct(productID)
+    }
+
+    override suspend fun getProductsFromBag(): List<BagProduct> {
+       return productRoomSource.getProductsFromBag()
+    }
+
+    override fun getProductCount(): Flow<Int> {
+        return productRoomSource.getProductQuantity()
+    }
+
+
+    override fun getProductAmountBag(): Flow<Double?> {
+        return productRoomSource.getProductAmount()
+    }
+
+    override suspend fun clearBasketProductTable() {
+        productRoomSource.clearBasketProductTable()
+    }
+
+    override suspend fun clearLikedProductsTable() {
+        productRoomSource.clearLikedProductsTable()
+    }
+
+
 }
--- app/src/main/java/com/example/momentcommerce/data/repo/ProductsRepository.kt
+++ app/src/main/java/com/example/momentcommerce/data/repo/ProductsRepository.kt
@@ -1,8 +1,31 @@
 package com.example.momentcommerce.data.repo
 
+import com.example.momentcommerce.model.BagProduct
+import com.example.momentcommerce.model.LikedProduct
 import com.example.momentcommerce.model.Product
+import kotlinx.coroutines.flow.Flow
 
 interface ProductsRepository {
 
     suspend fun getProducts() : List<Product>
+
+    suspend fun insertProduct(productBag : BagProduct)
+
+    suspend fun insertLikedProduct(likedProduct: LikedProduct)
+
+    suspend fun deleteLikedProduct(productID: Int)
+
+    suspend fun updateProduct(productID : Int, newProductCount : Int,newProductAmount : Double)
+
+    suspend fun deleteProduct(productID: Int)
+
+    suspend fun getProductsFromBag() : List<BagProduct>
+
+    fun getProductCount() : Flow<Int>
+
+    fun getProductAmountBag() : Flow<Double?>
+
+    suspend fun clearBasketProductTable()
+
+    suspend fun clearLikedProductsTable()
 }
