ae3696168d62ee26893ce8b01ff9e60b47f022b6
Ahmet Birsen <ahmetbrsn44@hotmail.com>
2024-02-12 23:03:03 +0300

    Fragment factory deleted.

--- app/src/main/java/com/example/momentcommerce/view/MainActivity.kt
+++ app/src/main/java/com/example/momentcommerce/view/MainActivity.kt
@@ -3,18 +3,13 @@ package com.example.momentcommerce.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.momentcommerce.R
-import com.example.momentcommerce.MomentFragmentFactory
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var fragmentFactory: MomentFragmentFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportFragmentManager.fragmentFactory = fragmentFactory
         setContentView(R.layout.activity_main)
     }
 }
