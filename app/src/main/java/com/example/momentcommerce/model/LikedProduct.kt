328c484fae33bba1a72cf5813ff33b7a88306407
Ahmet Birsen <ahmetbrsn44@hotmail.com>
2024-02-12 14:25:54 +0300

    LikeProdut entity created.

---
+++ app/src/main/java/com/example/momentcommerce/model/LikedProduct.kt
@@ -0,0 +1,12 @@
+package com.example.momentcommerce.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "LIKED_PRODUCTS")
+data class LikedProduct(
+    @PrimaryKey(autoGenerate = true)
+    val id :Long? = null,
+    val productID : Int ?= null
+)
